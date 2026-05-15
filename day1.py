6965707f14021545774208ecefa5badf2b4402b4
blackmamba-designs <anahowsthejosh@gmail.com>
2026-05-15 11:23:34 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Calculator!!")
+city = input("What city do you live in?\n")
+pet = input("What is your pet's name?\n")
+print("Your Band name is: " + city + " " + pet)
